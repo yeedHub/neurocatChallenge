20c0d7c3607c46c71b4a034506bf76e2fdec7d2b
y030 <yigit.akcay@icloud.com>
2020-09-27 17:59:37 +0300

    Using ResNet44 pretrained model with CIFAR10 dataset. Created Interface: split up model and data handdler.

---
+++ PyTorchModel.py
@@ -0,0 +1,56 @@
+from ModelInterface import NNModelInterface
+from ModelInterface import DataHandlerInterface
+
+import torch
+import torchvision.utils as utils
+import numpy as np
+import matplotlib.pyplot as plt
+
+class PyTorchClassifierDataHandler(DataHandlerInterface):
+  def __init__(self, dataset, params):
+    self.__dataset    = dataset[0] # contains x and y_true
+    self.__charlabels = dataset[1] # human readable labels
+    self.configureDataLoader(params)
+    self.__dataiter = iter(self.__loader)
+    
+  def configureDataLoader(self, params):
+    self.__loader = torch.utils.data.DataLoader(self.__dataset, **params)
+    
+  def getNextData(self, count):
+    samples = []
+    labels  = []
+    
+    for _ in range(count):
+      datapoint = self.__dataiter.next()
+      samples  += datapoint[0]
+      labels   += datapoint[1]
+      
+    return (samples, labels)
+  
+  def datasetLength(self):
+    return self.__dataset.__len__()
+
+class PyTorchNNModel(NNModelInterface):
+  def __init__(self, model):
+    self.__model = model
+  
+  def forward(self, samples):
+    self.__model.eval()
+    for sample in samples:
+      output = self.__model(sample)
+      # output = torch.nn.functional.softmax(output, dim=0)
+      # print(np.argmax(output.detach().numpu(), axis=1))
+      print(output)
+  
+  def onePassAnalysis(self, samples):
+    pass
+  
+  def multPassAnalysis(self, samples):
+    pass
+    
+
+# Small helper function to plot images
+def visualizeImages(imgs):
+  npimg = utils.make_grid(imgs).numpy()
+  plt.imshow(np.transpose(npimg, (1, 2, 0)))
+  plt.show()
--- eval.py
+++ eval.py
@@ -1,4 +1,4 @@
-import numpy as np
+from collections import OrderedDict
 
 import torch
 import torchvision.transforms as transforms
@@ -6,30 +6,50 @@ import torchvision.models as models
 import torchvision.datasets as datasets
 import torchvision.utils as utils
 
-import matplotlib.pyplot as plt
+from PyTorchModel import PyTorchNNModel 
+from PyTorchModel import PyTorchClassifierDataHandler
+from PyTorchModel import visualizeImages
 
-mobilenet = models.mobilenet_v2(pretrained=True)
-mobilenet.eval()
+import external.resnet as resnet
+
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.ToTensor()
 ])
 
-tinydata    = datasets.ImageFolder("./data/imagenet/", transform=preprocess)
-data_loader = torch.utils.data.DataLoader(tinydata, batch_size=3, shuffle=True, num_workers=4)
+cifar10 = datasets.CIFAR10("./data/CIFAR10/", transform=preprocess, download=False)
+cifar10_classes = ["airplane",
+                   "automobile",
+                   "bird",
+                   "cat",
+                   "deer",
+                   "dog",
+                   "frog",
+                   "horse",
+                   "ship",
+                   "truck"]
 
-dataiter = iter(data_loader)
-images, labels = dataiter.next()
+# Load the pre-trained checkpoint for the ResNet44 model (trained on CIFAR10)
+checkpoint = torch.load("./external/pretrained_models/resnet44-014dd654.th", map_location=torch.device('cpu'))
 
-def imshow(img):
-  npimg = img.numpy()
-  plt.imshow(np.transpose(npimg, (1, 2, 0)))
-  plt.show()
+# The model was saved wrapped in torch.nn.DataParallel, therefore we have to strip the "module." prefix
+new_state_dict = OrderedDict()
+for k, v in checkpoint['state_dict'].items():
+  name = k[7:] # remove "module."
+  new_state_dict[name] = v
+  
+# Load in the state_dict of the pre-trained model
+resnet44model = resnet.resnet44().load_state_dict(new_state_dict)
 
-output = mobilenet(images)#.unsqueeze(0))
-normalized = torch.nn.functional.softmax(output, dim=0)
-print(np.argmax(normalized.detach().numpy(), axis=1))
-
-imshow(utils.make_grid(images))
+# Our abstract model
+loaderparams = {
+  "batch_size": 3,
+  "shuffle": True,
+  "num_workers": 4
+}
+model = PyTorchNNModel(resnet44model)
+data  = PyTorchClassifierDataHandler([cifar10, cifar10_classes], loaderparams) 
+x, ytrue = data.getNextData(1)
+visualizeImages(x)
+print(data.datasetLength())
